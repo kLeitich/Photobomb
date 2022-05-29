4a57844a925eac3457f66b1f876a11590b6cde90
kleitich <kevleitich@gmail.com>
2022-05-30 02:38:08 +0300

    adding image models

--- bomb/models.py
+++ bomb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.model):
+    image=models.ImageField(upload_to = 'index/')
+    name= models.CharField(max_length =30)
+    description= models.CharField(max_length =300)
+    location=models.ForeignKey('Location')
+    category=models.ForeignKey('Catergory')
