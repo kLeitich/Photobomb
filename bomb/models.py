065bd174a2533d06da2440637bf14e8e6f4e650a
kleitich <kevleitich@gmail.com>
2022-05-30 02:45:04 +0300

    adding methods to save an image

--- bomb/models.py
+++ bomb/models.py
@@ -1,3 +1,5 @@
+from email.mime import image
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -7,3 +9,12 @@ class Image(models.model):
     description= models.CharField(max_length =300)
     location=models.ForeignKey('Location')
     category=models.ForeignKey('Catergory')
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save(self)
+
+    def filter_category(cls):
+        image=cls.objects.filter(category)
