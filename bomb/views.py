91955b32c64dd34f60efbf561e792cdb2a208f6a
kleitich <kevleitich@gmail.com>
2022-05-30 06:49:44 +0300

    adding a few

--- bomb/views.py
+++ bomb/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from bomb.models import Image
+
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    images=Image.objects.all()
+    return render(request, 'index.html',{'images':images})
 
 def food(request):
     return render(request, 'food.html')
