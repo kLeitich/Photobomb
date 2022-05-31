f4fcbedf0fc9e2430d40f23f4f6560e0cbd7f941
kleitich <kevleitich@gmail.com>
2022-05-31 08:02:18 +0300

    deploy

--- bomb/models.py
+++ bomb/models.py
@@ -1,10 +1,9 @@
-from unicodedata import category
 from django.db import models
 
 # Create your models here.
 
 class Location(models.Model):
-    location_name=models.CharField(max_length =30)
+    Location_name=models.CharField(max_length =30)
 
     def __str__(self):
         return self.Location_name
